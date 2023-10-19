584b57c736298813b5f1008a47940785f1aeb48c
slchangtw <shunlung.chang@gmail.com>
2023-10-19 10:38:05 +0800

    Add typing hints

--- src/perceptron.py
+++ src/perceptron.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 
 
 class Perceptron(nn.Module):
-    def __init__(self, input_dim):
+    def __init__(self, input_dim: int) -> None:
         super(Perceptron, self).__init__()
         self.fc1 = nn.Linear(input_dim, 1)
 
-    def forward(self, x_in):
+    def forward(self, x_in: torch.tensor) -> torch.tensor:
         return torch.sigmoid(self.fc1(x_in)).squeeze()
